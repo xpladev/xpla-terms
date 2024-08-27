0805e158bcee296ca4772a941e44bcdc9061c566
nglsena0722c <nglsena0722@com2us.com>
2024-08-27 15:59:44 +0900

    Feat : Add hello-pal privacy-policy

--- src/components/HelloPal/PolicySidebar.tsx
+++ src/components/HelloPal/PolicySidebar.tsx
@@ -1,10 +1,12 @@
 import clsx from "clsx";
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import useLanguage from "../../zustand/useLanguage";
 
 const HelloPalPolicySidebar = () => {
     const location = useLocation();
     const { pathname } = location;
+    const { language } = useLanguage();
 
     return <div className="md:mt-0 flex ">
         <Link
@@ -17,10 +19,9 @@ const HelloPalPolicySidebar = () => {
         </Link>
         <div className="border-r-[0.7px]  border-[#878D96] mr-[12px]" />
         <Link
-            className={clsx("text-[16px] font-medium leading-[19px] mr-[12px]",
-                pathname === "/" ? "text-[#00B1FF]" : "text-[#878D96]"
-            )}
-            to="/"
+            className={clsx("text-[16px] font-medium leading-[19px] mr-[12px] text-[#878D96]")}
+            target="_blank" rel="noopener noreferrer"
+            to={language === "korean" ? "https://terms.withhive.com/terms/policy/view/M604/T3" : "https://terms.withhive.com/terms/policy/view/M602/T3"}
         >
             Privacy Policy
         </Link>
